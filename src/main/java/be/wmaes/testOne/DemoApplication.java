8f58149030b145cd2f48602a2e35374250f546fc
Jenkins-X <tutorial@jenkins-x.io>
2018-11-17 12:15:45 +0100

    Initial import

---
+++ src/main/java/be/wmaes/testOne/DemoApplication.java
@@ -0,0 +1,12 @@
+package be.wmaes.testOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
